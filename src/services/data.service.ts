2893a29564df845f2d9224a2f2212cc362c23bd0
Dmitry Seregin <dmitry.seregin.7839@gmail.com>
2025-01-12 12:15:24 +0100

    Icon & non-root hosting support

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -8,6 +8,6 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
 
   getData(): Observable<Schedule> {
-    return this.httpClient.get<Schedule>('/train-data.json');
+    return this.httpClient.get<Schedule>('train-data.json');
   }
 }
